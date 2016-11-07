dc21c26f2b9a60b015d802821f315dc5806e5222
Feather <feather@netflix.com>
2016-11-07 13:27:06 -0800

    update to use ES6 syntax

--- src/movie-search.js
+++ src/movie-search.js
@@ -2,37 +2,15 @@ import React, { Component } from 'react';
 import Select from 'react-select';
 import fetch from 'isomorphic-fetch';
 
-
-const MovieSearch = React.createClass({
-  displayName: 'MovieSearch',
-  propTypes: {
-    label: React.PropTypes.string,
-  },
-  getInitialState () {
-    return {
-      backspaceRemoves: true,
-      multi: false
-    };
-  },
-  onChange (value) {
-    console.log('value: ', value);
-    this.setState({
-      value: value,
-    });
-  },
-  switchToMulti () {
-    this.setState({
-      multi: true,
-      value: [this.state.value],
-    });
-  },
-  switchToSingle () {
-    this.setState({
-      multi: false,
-      value: this.state.value ? this.state.value[0] : null
-    });
-  },
-  getUsers (input) {
+class MovieSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {movie: ""};
+  }
+  onChange(value) {
+    this.setState({ movie: value });
+  }
+  getUsers(input) {
     if (!input) {
       return Promise.resolve({ options: [] });
     }
@@ -42,38 +20,26 @@ const MovieSearch = React.createClass({
       .then((json) => {
         return { options: json.items };
       });
-  },
-  gotoUser (value, event) {
+  }
+  // TODO put hook to load page that comes after movie search here
+  gotoUser(value, event) {
     window.open(value.html_url);
-  },
-  toggleBackspaceRemoves () {
-    this.setState({
-      backspaceRemoves: !this.state.backspaceRemoves
-    });
-  },
-  toggleCreatable () {
-    this.setState({
-      creatable: !this.state.creatable
-    });
-  },
-  render () {
-    const AsyncComponent = this.state.creatable
-      ? Select.AsyncCreatable
-      : Select.Async;
+  }
 
+  render () {
     return (
       <div className="section">
         <h3 className="section-heading">{this.props.label}</h3>
-        <AsyncComponent multi={false}
-                        value={this.state.value}
-                        onChange={this.onChange}
-                        onValueClick={this.gotoUser}
-                        valueKey="id" labelKey="login"
-                        loadOptions={this.getUsers} backspaceRemoves={this.state.backspaceRemoves} />
+        <Select.Async multi={false}
+                      value={this.state.movie}
+                      onChange={this.onChange.bind(this)}
+                      onValueClick={this.gotoUser}
+                      valueKey="id" labelKey="login"
+                      loadOptions={this.getUsers} backspaceRemoves={true} />
 
       </div>
     );
   }
-});
+}
 
 export default MovieSearch;
