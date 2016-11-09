2b48c9e5e44783c176b7b39868e015982ade69f2
Feather <feather@netflix.com>
2016-11-09 12:28:36 -0800

    start to build out routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import MovieSearch from './movie-search';
-import TopNavbar from './top-navbar';
+//import TopNavbar from './top-navbar';
+import { Link } from 'react-router';
+import logo from './owl.png';
+import './styles/top-navbar.css';
 
 import 'react-select/dist/react-select.css';
 import './App.css';
 
 class App extends Component {
 
-  logChange(val) {
-    console.log("Selected: " + val);
-  }
-
   render() {
     return (
       <div className="App">
-        <TopNavbar />
+        <div className="top-navbar">
+            <ul role="nav">
+              <li><Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link></li>
+              <li><Link to="/">Minerva</Link></li>
+              <li><Link to="/lists">My Lists</Link></li>
+              <li><Link to="/labs">Athena Labs</Link></li>
+              <li><Link to="/jobs">Jobs</Link></li>
+            </ul>
+          </div>
         <div className="search-bar">
           <MovieSearch />
         </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './App';
+import MyLists from './my-lists';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="/lists" component={MyLists} />
+  </Router>
+  ), document.getElementById('root')
 );
